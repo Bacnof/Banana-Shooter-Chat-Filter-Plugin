34c8e12ab87a4929d65eebe1b63006012a7d8774
Bacnof <leopold.a.schubert@gmail.com>
2026-05-31 13:28:47 +0200

    Update Chat Filter.lua
    Finished Chat Filter

--- Chat Filter.lua
+++ Chat Filter.lua
@@ -1,3 +1,31 @@
 hook:Add("PlayerSay","Test",function(player,text)
 
+    local function loadWordsFromFile(filename)
+        local words = {}
+        for line in io.lines(filename) do
+            words[line:lower()] = true
+        end
+        return words
+    end
+
+
+    local function replaceWords(inputString, wordList)
+        local output = inputString:gsub("%w+", function(word)
+            if wordList[word:lower()] then
+                return "banana"
+            else
+                return word
+            end
+        end)
+        return output
+    end
+
+
+    local blacklist = loadWordsFromFile("Blacklist.txt")
+    local text = text
+
+    local result = replaceWords(text, blacklist)
+
+    return result
+
 end)
